cdcc07e9895c490ac56798551f08d153ecd9c718
MPDR <mpdr200011@gmail.com>
2019-03-29 18:54:07 +0000

    dijkstra pt1

---
+++ cal_fp05/src/Graph.h
@@ -0,0 +1,251 @@
+/*
+ * Graph.h
+ */
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include <vector>
+#include <queue>
+#include <list>
+#include <limits>
+#include <cmath>
+#include "MutablePriorityQueue.h"
+
+using namespace std;
+
+template <class T> class Edge;
+template <class T> class Graph;
+template <class T> class Vertex;
+
+#define INF std::numeric_limits<double>::max()
+
+/************************* Vertex  **************************/
+
+template <class T>
+class Vertex {
+	T info;                // contents
+	vector<Edge<T> > adj;  // outgoing edges
+	bool visited;          // auxiliary field
+	double dist = 0;
+	Vertex<T> *path = NULL;
+	int queueIndex = 0; 		// required by MutablePriorityQueue
+
+	bool processing = false;
+	void addEdge(Vertex<T> *dest, double w);
+
+public:
+	Vertex(T in);
+	bool operator<(Vertex<T> & vertex) const; // // required by MutablePriorityQueue
+	T getInfo() const;
+	double getDist() const;
+	Vertex *getPath() const;
+	friend class Graph<T>;
+	friend class MutablePriorityQueue<Vertex<T>>;
+};
+
+
+template <class T>
+Vertex<T>::Vertex(T in): info(in) {}
+
+/*
+ * Auxiliary function to add an outgoing edge to a vertex (this),
+ * with a given destination vertex (d) and edge weight (w).
+ */
+template <class T>
+void Vertex<T>::addEdge(Vertex<T> *d, double w) {
+	adj.push_back(Edge<T>(d, w));
+}
+
+template <class T>
+bool Vertex<T>::operator<(Vertex<T> & vertex) const {
+	return this->dist < vertex.dist;
+}
+
+template <class T>
+T Vertex<T>::getInfo() const {
+	return this->info;
+}
+
+template <class T>
+double Vertex<T>::getDist() const {
+	return this->dist;
+}
+
+template <class T>
+Vertex<T> *Vertex<T>::getPath() const {
+	return this->path;
+}
+
+/********************** Edge  ****************************/
+
+template <class T>
+class Edge {
+	Vertex<T> * dest;      // destination vertex
+	double weight;         // edge weight
+public:
+	Edge(Vertex<T> *d, double w);
+	friend class Graph<T>;
+	friend class Vertex<T>;
+};
+
+template <class T>
+Edge<T>::Edge(Vertex<T> *d, double w): dest(d), weight(w) {}
+
+
+/*************************** Graph  **************************/
+
+template <class T>
+class Graph {
+	vector<Vertex<T> *> vertexSet;    // vertex set
+
+public:
+	Vertex<T> *findVertex(const T &in) const;
+	bool addVertex(const T &in);
+	bool addEdge(const T &sourc, const T &dest, double w);
+	int getNumVertex() const;
+	vector<Vertex<T> *> getVertexSet() const;
+
+	// Fp05 - single source
+	void dijkstraShortestPath(const T &s);
+	void dijkstraShortestPathOld(const T &s);
+	void unweightedShortestPath(const T &s);
+	void bellmanFordShortestPath(const T &s);
+	vector<T> getPath(const T &origin, const T &dest) const;
+
+	// Fp05 - all pairs
+	void floydWarshallShortestPath();
+	vector<T> getfloydWarshallPath(const T &origin, const T &dest) const;
+
+};
+
+template <class T>
+int Graph<T>::getNumVertex() const {
+	return vertexSet.size();
+}
+
+template <class T>
+vector<Vertex<T> *> Graph<T>::getVertexSet() const {
+	return vertexSet;
+}
+
+/*
+ * Auxiliary function to find a vertex with a given content.
+ */
+template <class T>
+Vertex<T> * Graph<T>::findVertex(const T &in) const {
+	for (auto v : vertexSet)
+		if (v->info == in)
+			return v;
+	return NULL;
+}
+
+/*
+ *  Adds a vertex with a given content or info (in) to a graph (this).
+ *  Returns true if successful, and false if a vertex with that content already exists.
+ */
+template <class T>
+bool Graph<T>::addVertex(const T &in) {
+	if ( findVertex(in) != NULL)
+		return false;
+	vertexSet.push_back(new Vertex<T>(in));
+	return true;
+}
+
+/*
+ * Adds an edge to a graph (this), given the contents of the source and
+ * destination vertices and the edge weight (w).
+ * Returns true if successful, and false if the source or destination vertex does not exist.
+ */
+template <class T>
+bool Graph<T>::addEdge(const T &sourc, const T &dest, double w) {
+	auto v1 = findVertex(sourc);
+	auto v2 = findVertex(dest);
+	if (v1 == NULL || v2 == NULL)
+		return false;
+	v1->addEdge(v2,w);
+	return true;
+}
+
+
+/**************** Single Source Shortest Path algorithms ************/
+
+template<class T>
+void Graph<T>::dijkstraShortestPath(const T &origin) {
+    for (Vertex<T>* v: vertexSet) {
+    	v->dist = INF;
+    	v->path = nullptr;
+    }
+
+    Vertex<T>* start = findVertex(origin);
+    start->dist = 0;
+
+    MutablePriorityQueue<Vertex<T>> queue;
+    queue.insert(start);
+    while (!queue.empty()) {
+    	Vertex<T>* curr = queue.extractMin();
+
+    	for (const Edge<T>& e: curr->adj) {
+    		if (e.dest->getDist() > (curr->getDist() + e.weight)) {
+    			bool notInList = e.dest->getDist() == INF;
+
+    			e.dest->dist = curr->getDist() + e.weight;
+    			e.dest->path = curr;
+
+				if (notInList) {
+					queue.insert(e.dest);
+				} else {
+					queue.decreaseKey(e.dest);
+				}
+     		}
+    	}
+    }
+}
+
+template<class T>
+vector<T> Graph<T>::getPath(const T &origin, const T &dest) const{
+	vector<T> res;
+    Vertex<T>* start = findVertex(origin);
+	Vertex<T>* end = findVertex(dest);
+
+	if (start == nullptr || end == nullptr) {
+		return res;
+	}
+
+	Vertex<T> * curr = end;
+	while (curr != start) {
+		res.insert(res.begin(), curr->getInfo());
+		curr = curr->getPath();
+	}
+
+	res.insert(res.begin(), start->getInfo());
+
+	return res;
+}
+
+template<class T>
+void Graph<T>::unweightedShortestPath(const T &orig) {
+	// TODO
+}
+
+template<class T>
+void Graph<T>::bellmanFordShortestPath(const T &orig) {
+	// TODO
+}
+
+
+/**************** All Pairs Shortest Path  ***************/
+
+template<class T>
+void Graph<T>::floydWarshallShortestPath() {
+	// TODO
+}
+
+template<class T>
+vector<T> Graph<T>::getfloydWarshallPath(const T &orig, const T &dest) const{
+	vector<T> res;
+	// TODO
+	return res;
+}
+
+
+#endif /* GRAPH_H_ */
---
+++ cal_fp05/src/Test.cpp
@@ -0,0 +1,171 @@
+#include "cute/cute.h"
+#include "cute/ide_listener.h"
+#include "cute/xml_listener.h"
+#include "cute/cute_runner.h"
+#include <sstream>
+#include <random>
+#include <time.h>
+#include <chrono>
+#include "Graph.h"
+
+using namespace std;
+
+
+template <typename T1, typename T2>
+basic_ostream<char>& operator<<(basic_ostream<char> & strm, const pair<T1, T2>& kvPair)
+{
+		strm << "(" << kvPair.first << ", " << kvPair.second << ")";
+		return strm;
+}
+
+
+void geneateRandomGridGraph(int n, Graph<pair<int,int>> & g) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(1, n);
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			g.addVertex(make_pair(i,j));
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			for (int di = -1; di <= 1; di++)
+				for (int dj = -1; dj <= 1; dj++)
+					if ((di != 0) != (dj != 0) && i+di >= 0 && i+di < n && j+dj >= 0 && j+dj < n)
+							g.addEdge(make_pair(i,j), make_pair(i+di,j+dj), dis(gen));
+}
+
+void test_performance_dijkstra() {
+	for (int n = 10; n <= 100; n += 10) {
+		Graph< pair<int,int> > g;
+		cout << "Dijkstra generating grid " << n << " x " << n << " ..." << endl;
+		geneateRandomGridGraph(n, g);
+		cout << "Dijkstra processing grid " << n << " x " << n << " ..." << endl;
+		auto start = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				g.dijkstraShortestPath(make_pair(i,j));
+		auto finish = std::chrono::high_resolution_clock::now();
+		auto elapsed = chrono::duration_cast<chrono::microseconds>(finish - start).count();
+		cout << "Dijkstra processing grid " << n << " x " << n << " average time (micro-seconds)=" << (elapsed / (n*n)) << endl;
+	}
+}
+
+
+Graph<int> CreateTestGraph() {
+	Graph<int> myGraph;
+
+	for(int i = 1; i <= 7; i++)
+		myGraph.addVertex(i);
+
+	myGraph.addEdge(1, 2, 2);
+	myGraph.addEdge(1, 4, 7);
+	myGraph.addEdge(2, 4, 3);
+	myGraph.addEdge(2, 5, 5);
+	myGraph.addEdge(3, 1, 2);
+	myGraph.addEdge(3, 6, 5);
+	myGraph.addEdge(4, 3, 1);
+	myGraph.addEdge(4, 5, 1);
+	myGraph.addEdge(4, 6, 6);
+	myGraph.addEdge(4, 7, 4);
+	myGraph.addEdge(5, 7, 2);
+	myGraph.addEdge(6, 4, 3);
+	myGraph.addEdge(7, 6, 4);
+
+	return myGraph;
+}
+
+template <class T>
+void checkSinglePath(Graph<T> &g, vector<T> path, string expected) {
+	stringstream ss;
+	for(unsigned int i = 0; i < path.size(); i++)
+		ss << path[i] << " ";
+	ASSERT_EQUAL(expected, ss.str());
+}
+
+template <class T>
+void checkAllPaths(Graph<T> &g, string expected) {
+	stringstream ss;
+	vector<Vertex<T>* > vs = g.getVertexSet();
+	for(unsigned int i = 0; i < vs.size(); i++) {
+		ss << vs[i]->getInfo() << "<-";
+		if ( vs[i]->getPath() != NULL )
+			ss << vs[i]->getPath()->getInfo();
+		ss << "|";
+	}
+	ASSERT_EQUAL(expected, ss.str());
+}
+
+void test_unweightedShortestPath() {
+	Graph<int> myGraph = CreateTestGraph();
+
+	myGraph.unweightedShortestPath(3);
+	checkAllPaths(myGraph, "1<-3|2<-1|3<-|4<-1|5<-2|6<-3|7<-4|");
+	checkSinglePath(myGraph, myGraph.getPath(3, 7), "3 1 4 7 ");
+
+	myGraph.unweightedShortestPath(5);
+	checkSinglePath(myGraph, myGraph.getPath(5, 6), "5 7 6 ");
+}
+
+
+void test_bellmanFord() {
+	Graph<int> myGraph = CreateTestGraph();
+
+	myGraph.bellmanFordShortestPath(3);
+	checkAllPaths(myGraph, "1<-3|2<-1|3<-|4<-2|5<-4|6<-3|7<-5|");
+
+	myGraph.bellmanFordShortestPath(1);
+	checkSinglePath(myGraph, myGraph.getPath(1, 7), "1 2 4 5 7 ");
+
+	myGraph.bellmanFordShortestPath(5);
+	checkSinglePath(myGraph, myGraph.getPath(5, 6), "5 7 6 ");
+
+	myGraph.bellmanFordShortestPath(7);
+	checkSinglePath(myGraph, myGraph.getPath(7, 1), "7 6 4 3 1 ");
+}
+
+
+void test_dijkstra() {
+	Graph<int> myGraph = CreateTestGraph();
+
+	myGraph.dijkstraShortestPath(3);
+	checkAllPaths(myGraph, "1<-3|2<-1|3<-|4<-2|5<-4|6<-3|7<-5|");
+
+	myGraph.dijkstraShortestPath(1);
+	checkAllPaths(myGraph, "1<-|2<-1|3<-4|4<-2|5<-4|6<-4|7<-5|");
+	checkSinglePath(myGraph, myGraph.getPath(1, 7), "1 2 4 5 7 ");
+
+	myGraph.dijkstraShortestPath(5);
+	checkSinglePath(myGraph, myGraph.getPath(5, 6), "5 7 6 ");
+
+	myGraph.dijkstraShortestPath(7);
+	checkSinglePath(myGraph, myGraph.getPath(7, 1), "7 6 4 3 1 ");
+}
+
+void test_floydWarshall() {
+	Graph<int> myGraph = CreateTestGraph();
+	myGraph.floydWarshallShortestPath();
+	checkSinglePath(myGraph, myGraph.getfloydWarshallPath(1, 7), "1 2 4 5 7 ");
+	checkSinglePath(myGraph, myGraph.getfloydWarshallPath(5, 6), "5 7 6 ");
+	checkSinglePath(myGraph, myGraph.getfloydWarshallPath(7, 1), "7 6 4 3 1 ");
+}
+
+bool runAllTests(int argc, char const *argv[]) {
+	cute::suite s { };
+	s.push_back(CUTE(test_dijkstra));
+	s.push_back(CUTE(test_performance_dijkstra));
+	s.push_back(CUTE(test_bellmanFord));
+	s.push_back(CUTE(test_unweightedShortestPath));
+	s.push_back(CUTE(test_floydWarshall));
+	cute::xml_file_opener xmlfile(argc, argv);
+	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
+	auto runner = cute::makeRunner(lis, argc, argv);
+	bool success = runner(s, "AllTests");
+	return success;
+}
+
+int main(int argc, char const *argv[]) {
+    return runAllTests(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
